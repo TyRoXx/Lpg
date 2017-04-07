6592e6485bc31d37bc245db144861328efc1c0f1
TyRoXx <tyroxxxx@gmail.com>
2017-04-07 23:27:14 +0200

    Visual Leak Detector can be used with MSVC

--- tests/main.c
+++ tests/main.c
@@ -12,23 +12,34 @@
 #include "test_arithmetic.h"
 #include "test_stream_writer.h"
 #include "test_identifier.h"
+#if LPG_WITH_VLD
+#include <vld.h>
+static void check_allocations_maybe(void)
+{
+}
+#else
+static void check_allocations_maybe(void)
+{
+    check_allocations();
+}
+#endif
 
 int main(void)
 {
-    check_allocations();
+    check_allocations_maybe();
     test_integer();
-    check_allocations();
+    check_allocations_maybe();
     test_save_expression();
-    check_allocations();
+    check_allocations_maybe();
     test_unicode_string();
-    check_allocations();
+    check_allocations_maybe();
     test_allocator();
-    check_allocations();
+    check_allocations_maybe();
     test_arithmetic();
-    check_allocations();
+    check_allocations_maybe();
     test_stream_writer();
-    check_allocations();
+    check_allocations_maybe();
     test_identifier();
-    check_allocations();
+    check_allocations_maybe();
     return lpg_print_test_summary();
 }
