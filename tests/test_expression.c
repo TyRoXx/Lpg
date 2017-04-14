3f8f44ab915c886a341b3f7b06e130141e0e50ca
TyRoXx <tyroxxxx@gmail.com>
2017-04-14 12:46:29 +0200

    test call AST comparison

--- tests/test_expression.c
+++ tests/test_expression.c
@@ -1,15 +1,20 @@
 #include "test_expression.h"
 #include "lpg_expression.h"
 #include "test.h"
+#include "lpg_allocate.h"
 
 static void test_equal(expression left, expression right)
 {
     REQUIRE(expression_equals(&left, &right));
+    expression_free(&left);
+    expression_free(&right);
 }
 
 static void test_not_equal(expression left, expression right)
 {
     REQUIRE(!expression_equals(&left, &right));
+    expression_free(&left);
+    expression_free(&right);
 }
 
 void test_expression(void)
@@ -19,4 +24,40 @@ void test_expression(void)
                    expression_from_integer_literal(integer_create(0, 0)));
     test_not_equal(expression_from_integer_literal(integer_create(0, 1)),
                    expression_from_integer_literal(integer_create(0, 0)));
+    test_not_equal(expression_from_call(call_create(
+                       expression_allocate(expression_from_identifier(
+                           unicode_string_from_c_str("f"))),
+                       tuple_create(NULL, 0))),
+                   expression_from_call(call_create(
+                       expression_allocate(expression_from_identifier(
+                           unicode_string_from_c_str("g"))),
+                       tuple_create(NULL, 0))));
+    {
+        expression *arguments = allocate_array(1, sizeof(*arguments));
+        arguments[0] = expression_from_break();
+        test_not_equal(expression_from_call(call_create(
+                           expression_allocate(expression_from_identifier(
+                               unicode_string_from_c_str("f"))),
+                           tuple_create(NULL, 0))),
+                       expression_from_call(call_create(
+                           expression_allocate(expression_from_identifier(
+                               unicode_string_from_c_str("f"))),
+                           tuple_create(arguments, 1))));
+    }
+    {
+        expression *arguments_left = allocate_array(1, sizeof(*arguments_left));
+        arguments_left[0] = expression_from_break();
+        expression *arguments_right =
+            allocate_array(1, sizeof(*arguments_right));
+        arguments_right[0] =
+            expression_from_integer_literal(integer_create(0, 1));
+        test_not_equal(expression_from_call(call_create(
+                           expression_allocate(expression_from_identifier(
+                               unicode_string_from_c_str("f"))),
+                           tuple_create(arguments_left, 1))),
+                       expression_from_call(call_create(
+                           expression_allocate(expression_from_identifier(
+                               unicode_string_from_c_str("f"))),
+                           tuple_create(arguments_right, 1))));
+    }
 }
