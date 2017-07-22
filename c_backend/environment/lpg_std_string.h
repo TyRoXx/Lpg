ced423df6f060da28166d461f7a3ec3a84243a21
TyRoXx <tyroxxxx@gmail.com>
2017-07-22 15:38:14 +0200

    remove unnecessary member begin from string_ref

--- c_backend/environment/lpg_std_string.h
+++ c_backend/environment/lpg_std_string.h
@@ -8,22 +8,20 @@
 typedef struct string_ref
 {
     char const *data;
-    size_t begin;
     size_t length;
     size_t *references;
 } string_ref;
 
-static string_ref string_ref_create(char const *const data, size_t const begin,
-                                    size_t const length,
+static string_ref string_ref_create(char const *const data, size_t const length,
                                     size_t *const references)
 {
-    string_ref result = {data, begin, length, references};
+    string_ref result = {data, length, references};
     return result;
 }
 
 static string_ref string_literal(char const *const data, size_t const length)
 {
-    return string_ref_create(data, 0, length, NULL);
+    return string_ref_create(data, length, NULL);
 }
 
 static void string_ref_add_reference(string_ref const *const s)
@@ -56,8 +54,7 @@ static bool string_ref_equals(string_ref const left, string_ref const right)
     {
         return false;
     }
-    return !memcmp(
-        left.data + left.begin, right.data + right.begin, left.length);
+    return !memcmp(left.data, right.data, left.length);
 }
 
 static string_ref string_ref_concat(string_ref const left,
@@ -68,15 +65,14 @@ static string_ref string_ref_concat(string_ref const left,
     size_t *const references = (size_t *)allocation;
     *references = 1;
     string_ref const result = {
-        allocation + sizeof(*references), 0, result_length, references};
+        allocation + sizeof(*references), result_length, references};
     if (left.data)
     {
-        memcpy((char *)result.data, left.data + left.begin, left.length);
+        memcpy((char *)result.data, left.data, left.length);
     }
     if (right.data)
     {
-        memcpy((char *)result.data + left.length, right.data + right.begin,
-               right.length);
+        memcpy((char *)result.data + left.length, right.data, right.length);
     }
     return result;
 }
