367ada4c4ff034d7720ee2706397cfb696436877
TyRoXx <tyroxxxx@gmail.com>
2017-08-27 00:51:45 +0200

    try to fix GCC

--- generic/lpg_assert.h
+++ generic/lpg_assert.h
@@ -40,6 +40,9 @@
 #define LPG_TO_DO() abort()
 
 #define LPG_STATIC_ASSERT_IMPL3(X, L)                                          \
-    typedef char lpg_static_assertion_##L[(X) ? 1 : -1]
+    enum                                                                       \
+    {                                                                          \
+        lpg_static_assertion_##L = sizeof(char[1 - (2 * !(X))])                \
+    };
 #define LPG_STATIC_ASSERT_IMPL2(X, L) LPG_STATIC_ASSERT_IMPL3(X, L)
 #define LPG_STATIC_ASSERT(X) LPG_STATIC_ASSERT_IMPL2(X, __LINE__)
