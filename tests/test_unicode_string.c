04c157cc32680a0dc7bbee259d892388894851e9
TyRoXx <tyroxxxx@gmail.com>
2017-10-05 21:09:53 +0200

    test unicode_string_validate

--- tests/test_unicode_string.c
+++ tests/test_unicode_string.c
@@ -2,6 +2,7 @@
 #include "test.h"
 #include "lpg_unicode_string.h"
 #include <string.h>
+#include "lpg_unicode_view.h"
 
 void test_unicode_string(void)
 {
@@ -20,5 +21,25 @@ void test_unicode_string(void)
     {
         unicode_string s = unicode_string_from_c_str("abc");
         unicode_string_free(&s);
+    }
+    {
+        blob const raw = blob_from_c_str("a");
+        raw.data[0] = -1;
+        unicode_string validated = unicode_string_validate(raw);
+        REQUIRE(unicode_view_equals_c_str(unicode_view_from_string(validated), ""));
+        unicode_string_free(&validated);
+    }
+    {
+        blob const raw = blob_from_c_str("aaa");
+        raw.data[1] = -1;
+        unicode_string validated = unicode_string_validate(raw);
+        REQUIRE(unicode_view_equals_c_str(unicode_view_from_string(validated), "a"));
+        unicode_string_free(&validated);
+    }
+    {
+        blob const raw = blob_from_c_str("aaa");
+        unicode_string validated = unicode_string_validate(raw);
+        REQUIRE(unicode_view_equals_c_str(unicode_view_from_string(validated), "aaa"));
+        unicode_string_free(&validated);
     }
 }
