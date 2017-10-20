d2fca51c732401a7ba1135ceb393541ed84f6de4
TyRoXx <tyroxxxx@gmail.com>
2017-10-20 22:17:35 +0200

    test interface type mismatches

--- tests/test_semantic_errors.c
+++ tests/test_semantic_errors.c
@@ -448,6 +448,31 @@ void test_semantic_errors(void)
         REQUIRE(checked.functions[0].body.length == 0);
         checked_program_free(&checked);
     }
+    {
+        sequence root = parse("let i = interface\n"
+                              "let f = (a: i)\n"
+                              "f(1)");
+        semantic_error const errors[] = {
+            semantic_error_create(semantic_error_type_mismatch, source_location_create(2, 2))};
+        expected_errors expected = {errors, 1};
+        checked_program checked = check(root, std_library.globals, expect_errors, &expected);
+        REQUIRE(expected.count == 0);
+        sequence_free(&root);
+        REQUIRE(checked.function_count == 2);
+        checked_program_free(&checked);
+    }
+    {
+        sequence root = parse("let i = interface\n"
+                              "let e : i = 1\n");
+        semantic_error const errors[] = {
+            semantic_error_create(semantic_error_type_mismatch, source_location_create(1, 12))};
+        expected_errors expected = {errors, 1};
+        checked_program checked = check(root, std_library.globals, expect_errors, &expected);
+        REQUIRE(expected.count == 0);
+        sequence_free(&root);
+        REQUIRE(checked.function_count == 1);
+        checked_program_free(&checked);
+    }
     test_let_assignments(&std_library);
     standard_library_description_free(&std_library);
 }
