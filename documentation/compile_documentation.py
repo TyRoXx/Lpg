1188dda17ed0ef1b370d02e18c22c39c38eb3656
mamazu <maxo.pech@gmail.com>
2017-10-21 14:32:32 +0200

    Made the documentation compiler look nice

--- documentation/compile_documentation.py
+++ documentation/compile_documentation.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 
 
 def find_code(text):
@@ -10,27 +11,33 @@ def find_code(text):
     last_index = text.find(END_TAG, first_index + 1)
     return first_index + len(START_TAG), last_index
 
+
 def process_file(path):
     content = open(path, 'r').read()
     left, right = find_code(content)
     code_pieces = []
     while left is not None:
-        code_pieces.append(content[left:right])
+        code_pieces.append(content[left:right].strip())
         content = content[right:]
         left, right = find_code(content)
     return code_pieces
 
+
+def write_to_file(file_name, code):
+    for i, code_block in enumerate(code):
+        print_block = '// %s : %i\n%s\n' % (file_name, i, code_block)
+        destination.write(print_block)
+
+
+# Main Program
 root_dir = '.'
+destination = open('documentation.lpg', 'w')
 if len(sys.argv) == 2:
     root_dir = sys.argv[1]
 
-code = []
 for root, dirs, files in os.walk(root_dir):
     for fi in files:
         if fi.endswith('md'):
-            code += process_file(os.path.join(root, fi))
-
-code_file = open('documentation.lpg', 'w')
-for code_block in code:
-    code_file.write(code_block + '\n// Next block\n')
-
+            code = process_file(os.path.join(root, fi))
+            write_to_file(fi, code)
+destination.close()
