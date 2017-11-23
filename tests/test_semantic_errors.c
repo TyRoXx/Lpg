8f49a677d72bf1c0da7b5e6f0b6661b735d393ac
TyRoXx <tyroxxxx@gmail.com>
2017-11-23 21:27:06 +0100

    test tuple element access out of range

--- tests/test_semantic_errors.c
+++ tests/test_semantic_errors.c
@@ -280,6 +280,18 @@ void test_semantic_errors(void)
         checked_program_free(&checked);
     }
     {
+        sequence root = parse("let v = {}.9999999999999999999999999999999999999999\n");
+        semantic_error const errors[] = {
+            semantic_error_create(semantic_error_unknown_element, source_location_create(0, 11))};
+        expected_errors expected = {errors, 1};
+        checked_program checked = check(root, std_library.globals, expect_errors, &expected);
+        REQUIRE(expected.count == 0);
+        sequence_free(&root);
+        REQUIRE(checked.function_count == 1);
+        REQUIRE(checked.functions[0].body.length == 0);
+        checked_program_free(&checked);
+    }
+    {
         sequence root = parse("let v = {a}\n");
         semantic_error const errors[] = {
             semantic_error_create(semantic_error_unknown_element, source_location_create(0, 9))};
