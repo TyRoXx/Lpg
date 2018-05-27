8e51aa5f4a23b36a2da89e44b4a15b53bd6c7944
TyRoXx <tyroxxxx@gmail.com>
2018-05-27 15:43:59 +0200

    optimize the dummy GC so that the tests run more quickly

--- semantics/lpg_garbage_collector.c
+++ semantics/lpg_garbage_collector.c
@@ -3,12 +3,40 @@
 #include "lpg_stream_writer.h"
 #include "lpg_assert.h"
 
+enum
+{
+    block_size = 1024 * 10,
+    alignment = 16
+};
+
 void *garbage_collector_allocate(LPG_NON_NULL(garbage_collector *const gc), size_t const bytes)
 {
-    memory_allocation *const allocation = allocate(sizeof(*allocation) + bytes);
-    allocation->next = gc->allocations;
-    gc->allocations = allocation;
-    return &allocation->payload;
+    if (bytes == 0)
+    {
+        return NULL;
+    }
+    {
+        size_t const remaining_in_block = gc->blocks ? (gc->blocks->size - gc->blocks->used) : 0u;
+        if (remaining_in_block < bytes)
+        {
+            size_t new_block_size = block_size;
+            if (bytes > new_block_size)
+            {
+                new_block_size = bytes;
+            }
+            memory_block *const new_block = allocate(sizeof(*new_block) + new_block_size);
+            new_block->next = gc->blocks;
+            new_block->used = 0;
+            new_block->size = new_block_size;
+            gc->blocks = new_block;
+        }
+    }
+    void *const result = (gc->blocks->data + gc->blocks->used);
+    gc->blocks->used += bytes;
+    gc->blocks->used += (alignment - 1u);
+    gc->blocks->used &= ~(alignment - 1u);
+    ASSUME((gc->blocks->used % alignment) == 0);
+    return result;
 }
 
 void *garbage_collector_allocate_array(LPG_NON_NULL(garbage_collector *const gc), size_t const length,
@@ -21,10 +49,10 @@ void *garbage_collector_allocate_array(LPG_NON_NULL(garbage_collector *const gc)
 
 void garbage_collector_free(garbage_collector const gc)
 {
-    memory_allocation *i = gc.allocations;
+    memory_block *i = gc.blocks;
     while (i)
     {
-        memory_allocation *const next = i->next;
+        memory_block *const next = i->next;
         deallocate(i);
         i = next;
     }
--- semantics/lpg_garbage_collector.h
+++ semantics/lpg_garbage_collector.h
@@ -4,19 +4,21 @@
 
 typedef void (*garbage_collector_destructor)(void *);
 
-typedef struct memory_allocation
+typedef struct memory_block
 {
-    struct memory_allocation *next;
-    char payload[
+    struct memory_block *next;
+    size_t used;
+    size_t size;
+    char data[
 #ifdef _MSC_VER
         1
 #endif
     ];
-} memory_allocation;
+} memory_block;
 
 typedef struct garbage_collector
 {
-    memory_allocation *allocations;
+    memory_block *blocks;
 } garbage_collector;
 
 void *garbage_collector_allocate(LPG_NON_NULL(garbage_collector *const gc), size_t const bytes);
