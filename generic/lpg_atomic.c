b5124dbb0a8f68e54e209c3c26d3dfef729853f4
TyRoXx <tyroxxxx@gmail.com>
2018-07-14 12:26:14 +0200

    inline atomic operations on Windows

--- generic/lpg_atomic.c
+++ generic/lpg_atomic.c
@@ -1,24 +1,6 @@
 #include "lpg_atomic.h"
-#ifdef _MSC_VER
-#include <Windows.h>
-#endif
 
-#ifdef _MSC_VER
-void atomic_size_increment(size_t *atomic)
-{
-    InterlockedIncrementSizeT(atomic);
-}
-
-void atomic_size_decrement(size_t *atomic)
-{
-    InterlockedDecrementSizeT(atomic);
-}
-
-size_t atomic_size_load(size_t *atomic)
-{
-    return *(size_t volatile *)atomic;
-}
-#else
+#ifndef _WIN32
 void atomic_size_increment(size_t *atomic)
 {
     __atomic_add_fetch(atomic, 1, __ATOMIC_RELAXED);
--- generic/lpg_atomic.h
+++ generic/lpg_atomic.h
@@ -1,6 +1,24 @@
 #pragma once
 #include <stddef.h>
 
+#ifdef _WIN32
+#include <Windows.h>
+inline void atomic_size_increment(size_t *atomic)
+{
+    InterlockedIncrementSizeT(atomic);
+}
+
+inline void atomic_size_decrement(size_t *atomic)
+{
+    InterlockedDecrementSizeT(atomic);
+}
+
+inline size_t atomic_size_load(size_t *atomic)
+{
+    return *(size_t volatile *)atomic;
+}
+#else
 void atomic_size_increment(size_t *atomic);
 void atomic_size_decrement(size_t *atomic);
 size_t atomic_size_load(size_t *atomic);
+#endif
