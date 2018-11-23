163e1c80695090a2a946cbdcaee10922e5ff15dd
mamazu <maxo.pech@gmail.com>
2018-11-23 23:27:36 +0100

    Added tests for custom template resolution (#148)
    * Added tests for custom template resolution
    
    * Removed whitespace at end of line
    
    * Added newline at end of file

--- tests/test_web.c
+++ tests/test_web.c
@@ -1,4 +1,5 @@
 #include "test_web.h"
+#include "find_test_web_directory.h"
 #include "lpg_cli.h"
 #include "lpg_unicode_string.h"
 #include "website.h"
@@ -17,7 +18,33 @@ static void test_generate_main_html(void)
     memory_writer_free(&buffer);
 }
 
+static void test_generate_with_customized_template(void)
+{
+    memory_writer buffer = {NULL, 0, 0};
+    stream_writer writer = memory_writer_erase(&buffer);
+    unicode_string filepath = find_test_web_test_directory(unicode_view_from_c_str("test.lpg"));
+
+    unicode_string template_content = generate_template(writer, unicode_string_c_str(&filepath));
+    REQUIRE(buffer.used == 0);
+
+    REQUIRE(unicode_view_equals(unicode_view_from_string(template_content),
+                                unicode_view_from_c_str("<!DOCTYPE html>\n"
+                                                        "<html>\n"
+                                                        "<head>\n"
+                                                        "\t<title>Hello I am a custom template</title>\n"
+                                                        "</head>\n"
+                                                        "<body>\n"
+                                                        "\tVery custom\n"
+                                                        "</body>\n"
+                                                        "</html>")));
+
+    unicode_string_free(&filepath);
+    unicode_string_free(&template_content);
+    memory_writer_free(&buffer);
+}
+
 void test_web(void)
 {
     test_generate_main_html();
+    test_generate_with_customized_template();
 }
