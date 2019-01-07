2928888138d227e4be1b70f7cc6cae952b5291cd
mamazu <maxo.pech@gmail.com>
2019-01-07 19:52:35 +0100

    Removed another unused error

--- syntax/lpg_expression_parser.h
+++ syntax/lpg_expression_parser.h
@@ -16,7 +16,6 @@ typedef enum parse_error_type {
     parse_error_expected_element_name,
     parse_error_expected_identifier,
     parse_error_expected_lambda_body,
-    parse_error_expected_return_type,
     parse_error_unknown_binary_operator,
     parse_error_expected_parameter_list,
     parse_error_expected_for,
