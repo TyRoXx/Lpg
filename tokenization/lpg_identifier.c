3306720b2551689307b324707c045a3205e84a0b
TyRoXx <tyroxxxx@gmail.com>
2019-08-05 23:47:59 +0200

    inline commonly called tokenizer functions

--- tokenization/lpg_identifier.c
+++ tokenization/lpg_identifier.c
@@ -1,31 +1,5 @@
 #include "lpg_identifier.h"
 
-bool is_identifier_begin(char const c)
-{
-    if ((c >= 'a') && (c <= 'z'))
-    {
-        return 1;
-    }
-    if ((c >= 'A') && (c <= 'Z'))
-    {
-        return 1;
-    }
-    return (c == '_');
-}
-
-bool is_identifier_middle(char const c)
-{
-    if (is_identifier_begin(c))
-    {
-        return 1;
-    }
-    if ((c >= '0') && (c <= '9'))
-    {
-        return 1;
-    }
-    return false;
-}
-
 bool is_identifier(char const *data, size_t size)
 {
     if (size < 1)
--- tokenization/lpg_identifier.h
+++ tokenization/lpg_identifier.h
@@ -1,6 +1,30 @@
 #pragma once
 #include "lpg_unicode_string.h"
 
-bool is_identifier_begin(char const c);
-bool is_identifier_middle(char const c);
+static inline bool is_identifier_begin(char const c)
+{
+    if ((c >= 'a') && (c <= 'z'))
+    {
+        return 1;
+    }
+    if ((c >= 'A') && (c <= 'Z'))
+    {
+        return 1;
+    }
+    return (c == '_');
+}
+
+static inline bool is_identifier_middle(char const c)
+{
+    if (is_identifier_begin(c))
+    {
+        return 1;
+    }
+    if ((c >= '0') && (c <= '9'))
+    {
+        return 1;
+    }
+    return false;
+}
+
 bool is_identifier(char const *data, size_t size);
